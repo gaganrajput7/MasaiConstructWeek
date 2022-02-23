a8b70b56a144670b4699ce184309a4e7e5907aa5
P Naga <leadernaga90@gmail.com>
2022-02-24 02:30:29 +0530

    naga

---
+++ Doctors/Doctors.js
@@ -0,0 +1,258 @@
+var arr = [{
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/bdf0cf0d-754e-4254-aaf7-fdc7aedd7c35.jpg",
+        name: "Allergist and Clinical Immunologist",
+        description: "Manage allergies and treat immunity",
+        icon: "https://newassets.apollo247.com/images/ic_arrow_right.svg",
+        below: "Recurring infections, Immunity deficiency",
+
+
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/7236eab5-ea3c-4691-8362-c1fec2198596.jpg",
+        name: "Apollo Family Health Plan",
+        description: "Book Packages with your favorite doctors",
+        icon: "https://newassets.apollo247.com/images/ic_arrow_right.svg",
+        below: "Cold, Cough, Fever, Headache, Skin rashes, St"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_bariatrics.png",
+        name: "Bariatrics",
+        description: "For obesity-related disorders",
+        icon: "https://newassets.apollo247.com/images/ic_arrow_right.svg",
+        below: "Overweight, BMI over 30"
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/191336fb-173d-456b-b55b-c00669e35feb.jpg",
+        name: "Cardiology",
+        description: "For heart and blood pressure problems",
+        icon: "https://newassets.apollo247.com/images/ic_arrow_right.svg",
+        below: "Chest pain, Heart Failure, Cholesterol",
+
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/e5a03790-7cef-4f9c-909c-e45b658a5a6d.jpg",
+        name: "Cold, Cough and Fever",
+        description: "For common health concerns",
+        below: "Fever, Eye Infection, Stomach Ache, Headache"
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/62b70a73-837b-4b65-9b35-9b5ce04da79a.jpg",
+        name: "Colorectal Surgery",
+        description: "For disorders of rectum, anus, and colon",
+        below: "Inflammatory bowel disease, Fistula, Intestinal blockage"
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/34ae6abd-5a9d-40d1-98be-454ac94925c7.jpg",
+        name: "Counseling",
+        description: "Emotional or mental health difficulties",
+        below: "Depression, Anxiety, Mental Stress, Trauma"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/83163700-97e1-47cd-a715-5941fd708a03.png",
+        name: "Covid Recover Clinic",
+        description: "Treatment of Post-Covid Mid/long term illness",
+        below: "Post-Covid breathlessness, Chest pain"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_Critical_Care.png",
+        name: "Critical Care",
+        description: "For life-threatening conditions",
+        below: "ICU related, High-risk patients, Hypertension"
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/dc6baffd-4104-4a38-a8b2-0f262d7270aa.jpg",
+        name: "De-addiction Management",
+        description: "Specialists to help quit smoking, alcohol",
+        below: "Quit - Smoking, Drugs, Tobacco, Alcohol"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_Dentist.png",
+        name: "Dentist",
+        description: "Specialists for dental issues and treatments",
+        below: "Toothache, Mouth Ulcer, Crooked Teeth"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_diabetology.png",
+        name: "Diabetology",
+        description: "Managing all kinds of diabetes",
+        below: "Type 1 & Type 2 diabetes, Obesity, Anemia"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_dietician.png",
+        name: "Dietetics",
+        description: "Managing nutrition and diet regulation",
+        below: "Loss of appetite, Pregnancy Diet, Weight loss"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_ent.png",
+        name: "ENT",
+        description: "ENT specialists for Ear, Nose and Throat",
+        below: "Earache, Bad breath, Swollen neck, Vertigo"
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/9958ce34-1490-42da-a76d-f5304b873104.jpg",
+        name: "Endocrine Surgery",
+        description: "For surgeries of endocrine glands",
+        below: "Thyroid cancer, Adrenal gland tumours, Conn syndrome"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_diabetology.png",
+        name: "Endocrinology",
+        description: "For diabetes and hormonal problems",
+        below: "Thyroid, PCOS/PCOD, Growth issues"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_familyPhysician.png",
+        name: "Family Physician",
+        description: "For maintaining a healthy lifestyle",
+        below: "Routine check-ups, Health-risk assessments"
+    }, {
+        image: "https://newassets.apollo247.com/specialties/ic_gastroenterology.png",
+        name: "Gastroenterology/GI medicine",
+        description: "Managing digestive health",
+        below: "Vomiting, Acidity, Stomach ulcers, Swallowing"
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/4466dab4-c2b4-4961-b34e-c87673b1e9cd.jpg",
+        name: "General & Laparoscopic Surgeon",
+        description: "For general and minimally invasive surgeries",
+        below: "Cysts, Tumors, Appendicitis, Keyhole Surgery"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_General_Physician_Kavach.png",
+        name: "General Physician Kavach",
+        description: "For general and minimally invasive surgeries",
+        below: "Cysts, Tumors, Appendicitis, Keyhole Surgery"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_general_surgeon.png",
+        name: "General Surgery",
+        description: "For all kinds of surgeries",
+        below: "Hernia, Abdominal Surgery"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_haematology.png",
+        name: "Haematology",
+        description: "For diseases related to blood",
+        below: "Blood diseases"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_infectiousDiseases.png",
+        name: "Infectious Disease",
+        description: "For diseases caused by microorganisms",
+        below: "Severe headache, Fever, Animal bite"
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/e23814fe-e2e5-4ef3-9d18-65d2bb245440.jpg",
+        name: "Infertility",
+        description: "Specialists to help get pregnant",
+        below: "IVF, Miscarriages, Egg-freezing"
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/6b9f6dcb-8c1b-4da6-b9bb-6fd1294c8aaf.jpg",
+        name: "Lab Report Analysis",
+        description: "Understand your reports and treatments ",
+        below: "Blood Reports, Ultrasound Reports, MRI Scans"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_oncology.png",
+        name: "Medical Oncology",
+        description: "Cancer treatment using various techniques ",
+        below: "Immunotherapy, Hormonal therapy, Chemotherapy"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_nephrology.png",
+        name: "Nephrology",
+        description: "Managing all kidney related issues",
+        below: "Kidney Failure, Acute Renal Failure, Dialysis"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_neurology.png",
+        name: "Neurology",
+        description: "Managing issues of the nervous system, brain",
+        below: "Stroke, Dementia, Epilepsy, Movement issues"
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/362e133a-17cb-478f-a447-69bdf0bdddc0.jpg",
+        name: "Neurosurgery",
+        description: "For surgical treatment of nervous system disorders",
+        below: "Tumor, Seizures, Spine Surgery"
+    },
+    {
+        image: "https://prodaphstorage.blob.core.windows.net/specialties/c188f977-89d2-427c-8142-f67a47138462.jpg",
+        name: "Nuclear Medicine Physician",
+        description: "Using radioactive materials to treat issues",
+        below: "Scans- Breast, Brain, Bone, Thyroid, Kidney"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_obstetrics_and_gynaecology.png",
+        name: "Obstetrics & Gynaecology",
+        description: "For women health issues and surgeries",
+        below: "Irregular periods, Pregnancy, PCOD/PCOS"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_opthalmology.png",
+        name: "Ophthalmology",
+        description: "Ophthalmologist for eye care and surgeries",
+        below: "Cataract, Blurred vision, Glaucoma, Eye pain"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_OMSurgery.png",
+        name: "Oral & Maxillofacial Surgery",
+        description: "For reconstructive surgery of oral cavity, face, jaws",
+        below: "Teeth Removal, Cleft Lip, Cancer of neck, Jaw"
+    },
+    {
+        image: "https://newassets.apollo247.com/specialties/ic_Pain_Management.png",
+        name: "Pain Management",
+        description: "For management of all types of pain",
+        below: "Acute pain, Chronic pain, Cancer pain"
+    }
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+arr.map(function(elm) {
+    var div = document.createElement("div");
+    var div2 = document.createElement("div");
+    var image = document.createElement("img");
+    image.src = elm.image;
+    image.className = "caticon"
+    div2.append(image);
+    var div3 = document.createElement("div");
+    var name = document.createElement("p")
+    name.innerText = elm.name;
+    name.className = "name";
+    var description = document.createElement("p");
+    description.innerText = elm.description
+    description.className = "description";
+    var below = document.createElement("p");
+    below.innerText = elm.below;
+    below.className = "below";
+    div3.append(name, description, below);
+    var divicon = document.createElement("div");
+    var icon = document.createElement("img")
+    icon.src = "https://newassets.apollo247.com/images/ic_arrow_right.svg";
+    divicon.append(icon);
+    div.append(div2, div3, divicon)
+    document.querySelector(".category").append(div)
+
+
+})
